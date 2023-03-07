707a99c2f7e41e3e0bbf0c043e80a53c60a07c38
andres ibanez <andresibanezulloa@gmail.com>
2023-03-07 18:25:08 -0300

    added validations and asociations

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,3 +1,9 @@
 class Schedule < ApplicationRecord
+
+  #Asociaciones
   belongs_to :restaurant
+  
+  # Validaciones
+  validates :day, :open_at, :close_at, presence: true
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # Asociaciones
+  has_many :restaurants 
+
+  # Validaciones
+  validates :name, :address, presence: true
 end
